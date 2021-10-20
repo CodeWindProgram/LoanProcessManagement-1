d2d5d9036bdaa647741d5138f3bb36fe7eec86a4
Vishal <vishalgaichor954@gmail.com>
2021-10-21 02:05:07 +0530

    API API calling functionality from UI and API configurations

--- src/UI/LoanProcessManagement.App.Services/Services/Implementation/AccountService.cs
+++ src/UI/LoanProcessManagement.App.Services/Services/Implementation/AccountService.cs
@@ -1,8 +1,14 @@
-﻿using LoanProcessManagement.App.Services.Services.Interfaces;
+﻿using LoanProcessManagement.App.Services.Helper.APIHelper;
+using LoanProcessManagement.App.Services.Models.DTOs.ChangePassword;
+using LoanProcessManagement.App.Services.Services.Interfaces;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace LoanProcessManagement.App.Services.Services.Implementation
 {
@@ -10,30 +16,30 @@ namespace LoanProcessManagement.App.Services.Services.Implementation
     {
         private string BaseUrl = "";
         private HttpClient _client;
-        IOptions<APIDetails> _apiDetails;
+        IOptions<APIConfiguration> _apiDetails;
 
-        public AccountService(HttpClient client, IOptions<APIDetails> apiDetails)
+        public AccountService(HttpClient client, IOptions<APIConfiguration> apiDetails)
         {
             _client = client;
             _apiDetails = apiDetails;
         }
 
-        public async Task<Response<ChangePassword>> ChangePassword(ChangePassword changePassword)
+        public async Task<Response<ChangePasswordDTO>> ChangePassword(ChangePasswordDTO changePassword)
         {
-            BaseUrl = _apiDetails.Value.LoanProcessAPI;
+            BaseUrl = _apiDetails.Value.LoanProcessAPIUrl;
 
-            var model = new Response<ChangePassword>();
+            var model = new Response<ChangePasswordDTO>();
 
             var content = JsonConvert.SerializeObject(changePassword);
 
-            var httpResponse = await _client.PostAsync(BaseUrl + APIConstant.ChangePassword, new StringContent(content, Encoding.Default,
+            var httpResponse = await _client.PostAsync(BaseUrl + APIEndpoints.ChangePassword, new StringContent(content, Encoding.Default,
                 "application/json"));
 
             var jsonString = httpResponse.Content.ReadAsStringAsync().Result;
 
 
             var options = new JsonSerializerOptions();
-            model = System.Text.Json.JsonSerializer.Deserialize<Response<ChangePassword>>(jsonString, options);
+            model = System.Text.Json.JsonSerializer.Deserialize<Response<ChangePasswordDTO>>(jsonString, options);
 
             //JavaScriptSerializer js = new JavaScriptSerializer();
             //getStatusResponseModel = js.Deserialize<EMandateGetStatusResponseModel>(Finalresult);
@@ -43,3 +49,4 @@ namespace LoanProcessManagement.App.Services.Services.Implementation
             return model;
         }
     }
+}
