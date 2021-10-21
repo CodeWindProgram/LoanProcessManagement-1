429acf74d3b9df9a9744c8c2a0a301349a92189d
Vishal <vishalgaichor954@gmail.com>
2021-10-21 22:19:33 +0530

    Migration changes for Table relation

--- src/Core/LoanProcessManagement.Domain/Entities/LpmUserRoleMaster.cs
+++ src/Core/LoanProcessManagement.Domain/Entities/LpmUserRoleMaster.cs
@@ -1,5 +1,6 @@
 ﻿using LoanProcessManagement.Domain.Common;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -12,5 +13,6 @@ namespace LoanProcessManagement.Domain.Entities
         public long Id { get; set; }
         public string Rolename { get; set; }
         public bool Isactive { get; set; }
+        public ICollection<LpmUserMaster> LpmUserMasters { get; set; }
     }
 }
