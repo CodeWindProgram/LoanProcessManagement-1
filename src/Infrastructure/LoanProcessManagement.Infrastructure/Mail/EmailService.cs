836e8d38c7d4377a917b9635a79cef8a20a0601d
PratikshaPoshe <poshepratiksha612@gmail.com>
2021-10-29 19:46:35 -0700

    Changed method in email functionality - Pratiksha Poshe

--- src/Infrastructure/LoanProcessManagement.Infrastructure/Mail/EmailService.cs
+++ src/Infrastructure/LoanProcessManagement.Infrastructure/Mail/EmailService.cs
@@ -30,7 +30,7 @@ namespace LoanProcessManagement.Infrastructure.Mail
         //	commented by Pratiksha
         /// </summary>
         /// <param name="email">email of type Email</param>
-        public async Task SendEmail(Email email)
+        public async Task<bool> SendEmail(Email email)
         {
             var emailmsg = new MimeMessage();
             emailmsg.Sender = MailboxAddress.Parse(_emailSettings.Mail);
@@ -49,35 +49,8 @@ namespace LoanProcessManagement.Infrastructure.Mail
             }
 
             _logger.LogInformation("Email sent");
-
+            return true;
         } 
         #endregion
-
-        //public async Task<bool> SendEmail(Email email)
-        //{
-        //    var client = new SendGridClient(_emailSettings.ApiKey);
-
-        //    var subject = email.Subject;
-        //    var to = new EmailAddress(email.To);
-        //    var emailBody = email.Body;
-
-        //    var from = new EmailAddress
-        //    {
-        //        Email = _emailSettings.FromAddress,
-        //        Name = _emailSettings.FromName
-        //    };
-
-        //    var sendGridMessage = MailHelper.CreateSingleEmail(from, to, subject, emailBody, emailBody);
-        //    var response = await client.SendEmailAsync(sendGridMessage);
-
-        //    _logger.LogInformation("Email sent");
-
-        //    if (response.StatusCode == System.Net.HttpStatusCode.Accepted || response.StatusCode == System.Net.HttpStatusCode.OK)
-        //        return true;
-
-        //    _logger.LogError("Email sending failed");
-
-        //    return false;
-        //}
     }
 }
