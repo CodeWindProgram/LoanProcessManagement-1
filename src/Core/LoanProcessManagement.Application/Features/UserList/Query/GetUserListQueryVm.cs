5b8988e95cae9c0885ccb647a2baff8fc93eeb6a
Saif Khan <42858961+khansaif7887@users.noreply.github.com>
2021-10-30 17:17:17 +0530

    Pending Code Of API End Points - Saif Khan -30/10/2021

--- src/Core/LoanProcessManagement.Application/Features/UserList/Query/GetUserListQueryVm.cs
+++ src/Core/LoanProcessManagement.Application/Features/UserList/Query/GetUserListQueryVm.cs
@@ -7,12 +7,26 @@ namespace LoanProcessManagement.Application.Features.UserList.Query
 {
     public class GetUserListQueryVm
     {
-        public string LgId { get; set; }
+        public long Id { get; set; }
         public string EmployeeId { get; set; }
+        public string LgId { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
-        public LpmBranchMaster Branch { get; set; }
-        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+        public string SaltKey { get; set; }
         public string? StaffType { get; set; }
+        public long BranchId { get; set; }
+        public string BranchName { get; set; }
+        public string? SaleType { get; set; }
+        public string PhoneNumber { get; set; }
+        public long UserRoleId { get; set; }
+        public string UserRoleName { get; set; }
+        public string VerificationCode { get; set; }
+        public int? LeadCount { get; set; }
+        public int? WrongLoginAttempt { get; set; }
+        public bool IsLocked { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime? LastLogin { get; set; }
+        public DateTime? ActivatedOn { get; set; }
     }
 }
