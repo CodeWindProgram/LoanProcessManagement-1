c6792dd5e236f9d2e55655518801c456d119b53d
Vishal <vishalgaichor954@gmail.com>
2021-12-06 00:28:25 +0530

    Dynamic menu changes by vishal

---
+++ src/UI/LoanProcessManagement.App/Components/NavigationViewComponent.cs
@@ -0,0 +1,43 @@
+﻿using LoanProcessManagement.App.Services.Interfaces;
+using LoanProcessManagement.Application.Features.Menu.Query;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace LoanProcessManagement.App.Components
+{
+    public class NavigationViewComponent : ViewComponent
+    {
+        private readonly IMenuService _menuService;
+        public NavigationViewComponent(IMenuService menuService)
+        {
+            _menuService = menuService;
+        }
+
+
+        #region Invoke Async
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            GetMenuMasterServicesQuery menuProcess = new GetMenuMasterServicesQuery();
+
+            //Get the current claims principal
+            var identity = (ClaimsPrincipal)User;
+
+            var roleId = identity.Claims.FirstOrDefault(c => c.Type == "UserRoleId").Value;
+
+            menuProcess.UserRoleId = long.Parse(roleId);
+
+            var MenuServiceResponse = await _menuService.MenuProcess(menuProcess);
+
+            if (MenuServiceResponse != null && MenuServiceResponse.Succeeded && MenuServiceResponse.Data != null)
+            {
+                return await Task.Run(() => View(MenuServiceResponse)); //View(MenuServiceResponse);//.Data
+            }
+            return View();
+        } 
+        #endregion
+    }
+}
