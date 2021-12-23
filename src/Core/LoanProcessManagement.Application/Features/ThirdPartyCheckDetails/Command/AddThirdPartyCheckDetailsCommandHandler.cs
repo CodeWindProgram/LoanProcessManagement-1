a416d8413d63f064cb1ea6bf2d4c6982c10ed37f
PratikshaPoshe <poshepratiksha612@gmail.com>
2021-12-23 12:51:19 +0530

    ThirdPartyCheckDetails features - Pratiksha - 23/12/2021

---
+++ src/Core/LoanProcessManagement.Application/Features/ThirdPartyCheckDetails/Command/AddThirdPartyCheckDetailsCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using LoanProcessManagement.Application.Responses;
+using LoanProcessManagement.Domain.Entities;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LoanProcessManagement.Application.Features.ThirdPartyCheckDetails.Command
+{
+    //class AddThirdPartyCheckDetailsCommandHandler : IRequestHandler<AddThirdPartyCheckDetailsCommand, Response<AddThirdPartyCheckDetailsDto>>
+    //{
+    //    private readonly ILogger<AddThirdPartyCheckDetailsCommandHandler> _logger;
+    //    private readonly IMapper _mapper;
+
+
+    //    public AddThirdPartyCheckDetailsCommandHandler(ILogger<AddThirdPartyCheckDetailsCommandHandler> logger, IMapper mapper)
+    //    {
+    //        _logger = logger;
+    //        _mapper = mapper;
+    //    }
+
+    //    public Task<Response<AddThirdPartyCheckDetailsDto>> Handle(AddThirdPartyCheckDetailsCommand request, CancellationToken cancellationToken)
+    //    {
+    //        _logger.LogInformation("Handle Initiated");
+    //        var thirdPartyCheckDetails = _mapper.Map<LpmThirdPartyCheckDetails>(request);
+    //        _logger.LogInformation("Handle Completed");
+    //    }
+    //}
+}
---
+++ src/Core/LoanProcessManagement.Application/Features/ThirdPartyCheckDetails/Command/AddThirdPartyCheckDetailsDto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoanProcessManagement.Application.Features.ThirdPartyCheckDetails.Command
+{
+    class AddThirdPartyCheckDetailsDto
+    {
+        public long Id { get; set; }
+        public long? lead_Id { get; set; }
+        public long? valuerAgencyId { get; set; }
+        public DateTime ValuerDocumentOut_Date { get; set; }
+        public DateTime ValuerDocumentIn_Date { get; set; }
+        public string valuerAgencyDocuments { get; set; }
+        public string valuerAgencyComment { get; set; }
+        public int valuerAgencyStatus { get; set; }
+
+        public long? legalAgencyId { get; set; }
+        public DateTime LegalDocumentOut_Date { get; set; }
+        public DateTime LegalDocumentIn_Date { get; set; }
+        public string legalAgencyDocuments { get; set; }
+        public string legalAgencyComment { get; set; }
+        public int legalAgencyStatus { get; set; }
+
+        public long? fiAgencyId { get; set; }
+        public DateTime fiDocumentOut_Date { get; set; }
+        public DateTime fiDocumentIn_Date { get; set; }
+        public string fiAgencyDocuments { get; set; }
+        public string fiAgencyComment { get; set; }
+        public int fiAgencyStatus { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public string LastModifiedBy { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+    }
+}
