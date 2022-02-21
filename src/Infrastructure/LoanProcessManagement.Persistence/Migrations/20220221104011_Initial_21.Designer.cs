264829a6f1da64ec938a6612e15fb9da74f1dd4c
akshay9967 <46519290+akshay9967@users.noreply.github.com>
2022-02-21 18:45:31 +0530

    Added migration for LpmLeadMaster by - Akshay Pawar

--- src/Infrastructure/LoanProcessManagement.Persistence/Migrations/20220221104011_Initial_21.Designer.cs
+++ src/Infrastructure/LoanProcessManagement.Persistence/Migrations/20220221104011_Initial_21.Designer.cs
@@ -4,14 +4,16 @@ using LoanProcessManagement.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace LoanProcessManagement.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220221104011_Initial_21")]
+    partial class Initial_21
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -229,9 +231,6 @@ namespace LoanProcessManagement.Persistence.Migrations
 
                     b.Property<long?>("FormNoId")
                         .HasColumnType("bigint");
-
-                    b.Property<string>("FormNumber")
-                        .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("GstNo")
                         .HasColumnType("nvarchar(max)");
