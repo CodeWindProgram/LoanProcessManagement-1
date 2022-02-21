925c4699e275a9b6cb81dede79bbbc2a507447f2
RamyaGuduru538 <ramyaguduru18@gmail.com>
2022-02-21 17:59:12 +0530

    Credit Reports - Ramya

---
+++ src/API/LoanProcessManagement.Api/Controllers/v1/CreditCibilDetailsController.cs
@@ -0,0 +1,53 @@
+﻿using LoanProcessManagement.Application.Features.CreditCibilDetails.CreditCibilCheckDetails.Queries;
+using LoanProcessManagement.Application.Features.CreditCibilDetails.UserDetails.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoanProcessManagement.Api.Controllers.v1
+{
+    //[Route("api/[controller]")]
+    //[ApiController]
+    [ApiVersion("1")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class CreditCibilDetailsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly ILogger _logger;
+
+        public CreditCibilDetailsController(IMediator mediator, ILogger<CreditCibilDetailsController> logger)
+        {
+            _logger = logger;
+            _mediator = mediator;
+        }
+
+        #region Get cibil details List - Ramya Guduru - 15/02/2022
+        /// <summary>
+        /// 2022/15/02 -  Get All cibil details List API Call OK  
+        /// Commented By Ramya Guduru
+        /// </summary>
+        /// <returns>cibilDetails</returns>
+        [HttpGet("GetCreditCibilDetailsList")]
+
+        public async Task<ActionResult> GetCreditCibilDetailsList()
+        {
+            var res = await _mediator.Send(new GetCreditCibilDetailsQuery());
+            return Ok(res);
+        }
+
+        [HttpGet("GetCreditCibilUserDetailsList/{FormNo}")]
+
+        public async Task<ActionResult> GetCreditCibilUserDetailsList(string FormNo)
+        {
+            var res = await _mediator.Send(new GetCreditCibilUserDetailsQuery(FormNo));
+            return Ok(res);
+        }
+        #endregion
+    }
+}
